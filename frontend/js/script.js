f7aa3651586fe046b5b8e705f4a50304468fdbd5
Oltsu-code <123672634+Oltsu-code@users.noreply.github.com>
2024-03-23 14:58:40 +0200

    moved the project thing to project.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,20 +1,3 @@
-fetch('https://api.github.com/users/oltsu-code/repos')
-    .then(response => response.json())
-    .then(data => {
-        const projectsContainer = document.getElementById('project-container');
-
-        data.forEach(repo => {
-            const project = document.createElement('div');
-            project.innerHTML = `
-                <h3>${repo.name}</h3>
-                <p>${repo.description}</p>
-                <a href="${repo.html_url}" target="_blank">View on GitHub</a>
-            `;
-            projectsContainer.appendChild(project);
-        });
-    })
-    .catch(error => console.error('Error fetching GitHub repositories:', error));
-
 function toggleDropdown() {
     const dropdown = document.getElementById('dropdown-menu');
     if (dropdown.style.display === 'none' || dropdown.style.display.display == '') {
