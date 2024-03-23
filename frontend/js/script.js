330628424ca1db235aefca47a19ce29854ef51f1
Oltsu-code <123672634+Oltsu-code@users.noreply.github.com>
2024-03-23 14:08:53 +0200

    some stuff

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -14,3 +14,13 @@ fetch('https://api.github.com/users/oltsu-code/repos')
         });
     })
     .catch(error => console.error('Error fetching GitHub repositories:', error));
+
+function toggleDropdown() {
+    const dropdown = document.getElementById('dropdown-menu');
+    if (dropdown.style.display === 'none' || dropdown.style.display.display == '') {
+        dropdown.style.display = 'block';
+    } else {
+        dropdown.style.display = 'none';
+    }
+}
+
