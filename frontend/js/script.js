46803da994f3ffead7f2fea41886660c088958a5
Oltsu-code <123672634+Oltsu-code@users.noreply.github.com>
2024-04-27 23:27:40 +0300

    cleaned the code from some possible bugs

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,8 +1,9 @@
 function toggleDropdown() {
     const dropdown = document.getElementById('dropdown-menu');
-    if (dropdown.style.display === 'none' || dropdown.style.display.display == '') {
-        dropdown.style.display = 'block';
-    } else {
-        dropdown.style.display = 'none';
+    if (!dropdown) {
+        console.error('Element with id "dropdown-menu" not found');
+        return;
     }
+
+    dropdown.style.display = dropdown.style.display === 'none' || dropdown.style.display === '' ? 'block' : 'none';
 }
